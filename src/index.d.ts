6097627ac252bb0753660417e28aae86cc4d4de8
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-01-06 00:11:13 +0600

    .

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,18 @@
+// import { PrismaClient } from "@prisma/client";
+
+// export function createContext(ctx: any): Promise<Context>;
+
+// export * from ".prisma/client/index.d";
+
+// export interface Context {
+//     prisma: PrismaClient;
+// }
+
 import { PrismaClient } from "@prisma/client";
-
-export function createContext(ctx: any): Promise<Context>;
-
-export * from ".prisma/client/index.d";
+export { Prisma } from ".prisma/client";
 
 export interface Context {
     prisma: PrismaClient;
 }
+
+export function createContext(): Promise<Context>;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
+// import { PrismaClient } from "@prisma/client";
+// import { Context } from "./index.d";
+
+// const prisma = new PrismaClient();
+
+// export const createContext = async (ctx: any): Promise<Context> => {
+//     // Skip if you are not using a serverless environment
+//     ctx.callbackWaitsForEmptyEventLoop = false;
+
+//     return { ...ctx, prisma };
+// };
+
+// // export default prisma;
+
 import { PrismaClient } from "@prisma/client";
 import { Context } from "./index.d";
 
 const prisma = new PrismaClient();
 
-export { prisma };
-
-export const createContext = async (ctx: any): Promise<Context> => {
-    // Skip if you are not using a serverless environment
-    ctx.callbackWaitsForEmptyEventLoop = false;
-
-    return { ...ctx, prisma };
-};
-
-// export default prisma;
+export const createContext = async (): Promise<Context> => ({ prisma });
